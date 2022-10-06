1f8cdab2f5d9f0ab02d7a4c534931b5b547f4b15
Marcia Valerio R <84945072+marcys09@users.noreply.github.com>
2022-10-05 21:17:30 -0600

    Test

--- src/main/java/com/tienda/domain/Cliente.java
+++ src/main/java/com/tienda/domain/Cliente.java
@@ -1,23 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package com.tienda.domain;
 
 import java.io.Serializable;
 import javax.persistence.*;
 
+public class Cliente {
 
-@Data
-@Entity
+    @Data
+    @Entity
 
+    private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(Strategy = GenerationType.IDENTITY)
+    private Long idCliente;
 
-
-private static final long serialVersionUID = 1L;
-
-@Id
-@GeneratedValue (Strategy = GenerationType.IDENTITY)
-private Long idCliente;
-
-
-private String nombre;
-private String apellidos;
-private String correo;
-private String telefono;
+    private String nombre;
+    private String apellidos;
+    private String correo;
+    private String telefono;
+}
